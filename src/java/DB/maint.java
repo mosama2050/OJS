7663233df720b69b1ede4c05d89c6d9132b16ad6
mohama25 <immardoo@gmail.com>
2019-04-30 07:31:53 +0200

    frist commit

---
+++ src/java/DB/maint.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DB;
+
+import java.sql.Date;
+import model.User;
+import model.post;
+
+/**
+ *
+ * @author Nitro
+ */
+public class maint {
+
+    public static void main(String[] args) {
+        PostDOA o = new PostDOA();
+     //   post i = o.VIEWPOST("1");
+      
+    
+       
+
+      
+    }
+}
